2d333aa4c0fb3196b33dd2c60be6d887d23906a6
Luiz Guilherme <luizgmelo64@gmail.com>
2025-12-14 13:20:27 -0300

    feat: add gateway that send arduino info to mqtt server

---
+++ gateway.py
@@ -0,0 +1,27 @@
+import serial
+import paho.mqtt.client as mqtt
+import time
+import json
+
+PORT_SERIAL = '/dev/ttyUSB0'
+SPEED = 9600
+
+BROKER = "broker.hivemq.com"
+PORT_MQTT = 1883
+TOPIC = "class/test/temperature"
+
+arduino = serial.Serial(PORT_SERIAL, SPEED, timeout=1)
+time.sleep(2)
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+client.connect(BROKER, PORT_MQTT, 60)
+
+while True:
+    if arduino.in_waiting > 0:
+        line = arduino.readline().decode('utf-8').strip()
+        
+        if line.startswith("{") and line.endswith("}"):
+            data = json.loads(line)
+            client.publish(TOPIC, line)
+
+        client.publish(TOPIC, line)
